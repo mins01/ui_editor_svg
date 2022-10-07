3fd35072e38e96289d03eb0f54739c483f6dfbe3
mins01 <mins01.lycos.co.kr@gmail.com>
2022-10-07 09:19:31 +0900

    first

---
+++ assets/SvgEditor.js
@@ -0,0 +1,12 @@
+class SvgEditor{
+    constructor() {
+        super();
+        this.debug = false;
+        this.width = null;
+        this.height = null;
+        this.zoom = 1;
+        this.nodeEditorBox = null
+        this.rootSvg = null
+    }
+}
+export default SvgEditor;
